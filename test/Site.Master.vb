f049880dff965ecf45f09757add1df1548a3443a
Arron <arronkosasih@gmail.com>
2018-09-19 13:59:59 +0700

    perubahan pertama

--- test/Site.Master.vb
+++ test/Site.Master.vb
@@ -1,6 +1,6 @@
 ﻿Public Class SiteMaster
 	Inherits MasterPage
 	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-
-	End Sub
+        MsgBox("abc")
+    End Sub
 End Class
